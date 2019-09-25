5f9fe30652a6ed12fe24b24afb4a295497ff4968
CheeseB <2489ckckck@naver.com>
2019-09-25 23:44:18 +0900

    modify

--- BOJ1929(correct).java
+++ BOJ1929(correct).java
@@ -12,19 +12,21 @@ public class Main {
 		m = scan.nextInt();
 		n = scan.nextInt();
 		
-		boolean[] isSosu = new boolean[n+1];
-		isSosu[0] = true;
-		isSosu[1] = true;
+		boolean[] notSosu = new boolean[n+1];
+		notSosu[0] = true;
+		notSosu[1] = true;
 		
 		for(int i=2; i<=n; i++) {
+			if(notSosu[i] == true) continue;
+			
 			for(int j=2; j<=n; j++) {
 				if(i*j > n) break;
-				isSosu[i*j] = true;
+				notSosu[i*j] = true;
 			}
 		}
 
 		for(int k=m; k<=n; k++) {
-			if(isSosu[k] == false) System.out.println(k);
+			if(notSosu[k] == false) System.out.println(k);
 		}
 		
 	}
