6b475966c84451ff9a0a60a314e2a7983a872734
CheeseB <2489ckckck@naver.com>
2020-01-03 21:06:53 +0900

    number 2588

---
+++ Baekjoon Online Judge/BOJ2588.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	
+	int a, b, n3, n4, n5, n6;
+	
+	scanf("%d",&a);
+	scanf("%d",&b);
+	
+	n3 = a * (b%10);
+	n4 = a * ((b/10)%10);
+	n5 = a * (b/100);
+	n6 = n3 + n4*10 + n5*100;
+	
+	printf("%d\n",n3);
+	printf("%d\n",n4);
+	printf("%d\n",n5);
+	printf("%d\n",n6);
+	
+	return 0;
+}
