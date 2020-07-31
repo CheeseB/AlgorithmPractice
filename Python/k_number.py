cdca47586a53ba04818b62792da8636746c0ef82
CheeseB <2489ckckck@naver.com>
2020-07-31 16:40:19 +0900

    k_number

---
+++ Python/k_number.py
@@ -0,0 +1,11 @@
+t = int(input())
+
+for i in range(t):
+    n, s, e, k = map(int, input().split())
+    num_list = list(map(int, input().split()))
+
+    list_part = num_list[s-1:e]
+    list_part.sort()
+
+    print('#', i+1, sep='', end=' ')
+    print(list_part[k-1])
