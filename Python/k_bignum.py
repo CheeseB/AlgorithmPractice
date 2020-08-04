e828a4b6424ed8fb4ffeb0b59b88677eec5cca94
CheeseB <2489ckckck@naver.com>
2020-08-04 20:37:33 +0900

    modified

--- Python/k_bignum.py
+++ Python/k_bignum.py
@@ -1,12 +1,12 @@
 n, k = map(int, input().split())
 cards = list(map(int, input().split()))
-sum_list = list()
+sums = set()
 
 for a in range(n-2):
     for b in range(a+1, n-1):
         for c in range(b+1, n):
-            sum_list.append(cards[a] + cards[b] + cards[c])
+            sums.add(cards[a] + cards[b] + cards[c])
 
-sum_list = list(set(sum_list))
+sum_list = list(sums)
 sum_list.sort(reverse=True)
 print(sum_list[k-1])
