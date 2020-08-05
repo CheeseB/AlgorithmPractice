6851a621333372b99390df047720a1f442f9abab
CheeseB <2489ckckck@naver.com>
2020-08-05 12:13:32 +0900

    representative_value.py

--- Python/representative_value.py
+++ Python/representative_value.py
@@ -1,7 +1,16 @@
 n = int(input())
 scores = list(map(int, input().split()))
 
-sub = 0
+min_sub = 101
+result_num = 0
 avg = round(sum(scores) / n)
-for num, score in scores:
+
+for num, score in enumerate(scores):
     sub = abs(score - avg)
+    if sub <= min_sub:
+        if sub == min_sub and score <= scores[result_num]:
+            continue
+        min_sub = sub
+        result_num = num
+
+print(avg, result_num+1)
