1e45e9dbf4b64780900ad6065e47b1f23d72aa94
CheeseB <2489ckckck@naver.com>
2020-08-06 10:01:43 +0900

    modified

--- Python/regular_polyhedron.py
+++ Python/regular_polyhedron.py
@@ -1,11 +1,12 @@
 n, m = map(int, input().split())
 
-sum_list = [0] * 40
+sum_list = [0] * (n+m+3)
 for i in range(1, n+1):
     for j in range(1, m+1):
         sum_list[i+j] += 1
 
-results = [idx for idx, value in enumerate(sum_list) if value == max(sum_list)]
+max_sum = max(sum_list)
+results = [idx for idx, value in enumerate(sum_list) if value == max_sum]
 
 for result in results:
     print(result, end=' ')
