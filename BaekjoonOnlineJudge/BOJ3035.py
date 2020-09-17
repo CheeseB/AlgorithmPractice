d24431d2e9fbadc6a44b42220a2483a381086c71
CheeseB <2489ckckck@naver.com>
2020-09-17 16:45:54 +0900

    modified

--- BaekjoonOnlineJudge/BOJ3035.py
+++ BaekjoonOnlineJudge/BOJ3035.py
@@ -1,19 +1,14 @@
 r, c, zr, zc = map(int, input().split())
 
-arr = [[0] * c for _ in range(r)]
 for i in range(r):
     line = input()
-    for j in range(c):
-        arr[i][j] = line[j]
+    newLine = [0] * c * zc
 
-new_row = [0] * c * zc
-for i in range(r):
     idx = 0
     for j in range(c):
         for _ in range(zc):
-            new_row[idx] = arr[i][j]
+            newLine[idx] = line[j]
             idx += 1
+
     for _ in range(zr):
-        for word in new_row:
-            print(word, end='')
-        print()
+        print(''.join(newLine))
