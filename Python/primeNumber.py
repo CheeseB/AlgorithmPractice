ccd3e7bfb87dfbd342829e1996476e98eed8df2c
CheeseB <2489ckckck@naver.com>
2020-10-07 14:46:32 +0900

    a

--- Python/primeNumber.py
+++ Python/primeNumber.py
@@ -1,18 +1,11 @@
-import math
-
 maxNum = int(input())
 board = [0] * (maxNum + 1)
-
-for i in range(2, math.ceil(math.sqrt(maxNum)) + 1):
-    if board[i] == 0:
-        j = 2
-        while i * j <= maxNum:
-            board[i * j] = 1
-            j += 1
-
 answer = 0
+
 for i in range(2, maxNum + 1):
     if board[i] == 0:
         answer += 1
+        for j in range(i, maxNum + 1, i):
+            board[j] = 1
 
 print(answer)
