6a48d958388d0039ca0de88db816dc4c088433af
CheeseB <2489ckckck@naver.com>
2020-10-07 14:36:36 +0900

    a

--- Python/representativeValue.py
+++ Python/representativeValue.py
@@ -3,6 +3,13 @@ scores = list(map(int, input().split()))
 
 min_sub = 101
 result_num = 0
+
+# round_half_up이 우리가 쓰는 반올림 방법이지만
+# round 함수는 round_half_even 방식을 사용함.
+# 이는 반절의 수(5) 를 짝수로 반올림하는 방식임.
+# 예시> 4.500 = 4 , 4.501 = 5 , 5.500 = 6
+
+# 그러므로 파이썬에서 반올림을 할 땐 아래와 같이 써야함.
 avg = int((sum(scores) / n) + 0.5)
 
 for num, score in enumerate(scores):
