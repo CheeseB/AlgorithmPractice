2e7fc34acf4d385e9e7e4a28a56d76e11e5d8ca1
CheeseB <2489ckckck@naver.com>
2020-10-08 17:41:40 +0900

    modified

--- Python/diceGame.py
+++ Python/diceGame.py
@@ -2,15 +2,18 @@ person = int(input())
 maxPrice = 0
 
 for _ in range(person):
-    numList = list(map(int, input().split()))
-    if numList.count(numList[0]) == 3:
-        price = 10000 + (numList[0] * 1000)
-    elif numList.count(numList[0]) == 2:
-        price = 1000 + (numList[0] * 100)
-    elif numList.count(numList[1]) == 2:
-        price = 1000 + (numList[1] * 100)
+    dice = input().split()
+    dice.sort()
+
+    a, b, c = map(int, dice)
+    if a == b == c:
+        price = 10000 + (a * 1000)
+    elif a == b:
+        price = 1000 + (a * 100)
+    elif b == c:
+        price = 1000 + (b * 100)
     else:
-        price = max(numList) * 100
+        price = a * 100
 
     if price > maxPrice:
         maxPrice = price
