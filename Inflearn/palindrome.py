6ac0e93a8c91341b60149c0ccbe917320178b94e
CheeseB <2489ckckck@naver.com>
2020-10-09 20:57:13 +0900

    modified3

--- Inflearn/palindrome.py
+++ Inflearn/palindrome.py
@@ -2,17 +2,8 @@ tests = int(input())
 
 for test in range(tests):
     word = input().upper()
-    length = len(word)
 
-    # 파이썬은 인덱스 접근을 거꾸로 음수로도 할 수 있음!
-    # -1이 맨 끝 인덱스, 숫자가 낮아질수록 앞 인덱스로 감
-    #  0  1  2  3  4
-    # |--|--|--|--|--|
-    #  -5 -4 -3 -2 -1
-
-    for i in range(length // 2):
-        if word[i] != word[-1-i]:
-            print('#%d NO' % (test + 1))
-            break
-    else:
+    if word == word[::-1]:
         print('#%d YES' % (test + 1))
+    else:
+        print('#%d NO' % (test + 1))
