d7f333ee2789085d1213fe6a52e3f83ed0ce0673
CheeseB <2489ckckck@naver.com>
2020-10-09 20:42:28 +0900

    modified2

--- Inflearn/palindrome.py
+++ Inflearn/palindrome.py
@@ -3,8 +3,14 @@ tests = int(input())
 for test in range(1, tests + 1):
     print('#' + str(test), end=' ')
     word = input().upper()
+    length = len(word)
+    isPal = True
 
-    if word == word[::-1]:
+    for i in range(length // 2):
+        if word[i] != word[length - i - 1]:
+            isPal = False
+
+    if isPal:
         print('YES')
     else:
         print('NO')
