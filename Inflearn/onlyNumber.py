77d499a0e806a3e362b0184e0928e32891209523
CheeseB <2489ckckck@naver.com>
2020-10-18 19:28:58 +0900

    modi

--- Inflearn/onlyNumber.py
+++ Inflearn/onlyNumber.py
@@ -3,7 +3,9 @@ num = 0
 cnt = 0
 
 for x in line:
-    if x.isdigit():
+    if x.isdecimal():
+        # isdigit(): 모든 숫자 자료형 취급
+        # isdecimal(): 0 ~ 9까지만
         num = (num * 10) + int(x)
 
 for i in range(1, num + 1):
