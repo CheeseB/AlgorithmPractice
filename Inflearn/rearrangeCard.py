1652c19ffadabc57cd64ecaf5aee1402a7ff0d7f
CheeseB <2489ckckck@naver.com>
2020-10-20 14:17:18 +0900

    modify

--- Inflearn/rearrangeCard.py
+++ Inflearn/rearrangeCard.py
@@ -6,8 +6,7 @@ for _ in range(10):
     for i in range((b-a+1)//2):
         cards[a+i], cards[b-i] = cards[b-i], cards[a+i]
 
+cards.pop(0)
 for card in cards:
-    if card == 0:
-        continue
     print(card, end=' ')
 
