728e483874074539959b19cc84100444550109b5
CheeseB <2489ckckck@naver.com>
2020-10-21 16:47:45 +0900

    listjoin - join함수 안쓰고

--- Inflearn/listJoin.py
+++ Inflearn/listJoin.py
@@ -1,10 +1,27 @@
-_ = int(input())
+len1 = int(input())
 list1 = list(map(int, input().split()))
-_ = int(input())
+len2 = int(input())
 list2 = list(map(int, input().split()))
 
-res = list1 + list2
-res.sort()
+res = list()
+p1 = 0
+p2 = 0
+
+while p1 < len1 and p2 < len2:
+    if list1[p1] < list2[p2]:
+        res.append(list1[p1])
+        p1 += 1
+    else:
+        res.append(list2[p2])
+        p2 += 1
+
+while p1 < len1:
+    res.append(list1[p1])
+    p1 += 1
+
+while p2 < len2:
+    res.append(list2[p2])
+    p2 += 1
 
 for x in res:
     print(x, end=' ')
