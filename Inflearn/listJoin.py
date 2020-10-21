5c980e21b93ea19b824a9a11268d909c8821443d
CheeseB <2489ckckck@naver.com>
2020-10-21 16:52:37 +0900

    modify - 슬라이스 이용

--- Inflearn/listJoin.py
+++ Inflearn/listJoin.py
@@ -4,8 +4,7 @@ len2 = int(input())
 list2 = list(map(int, input().split()))
 
 res = list()
-p1 = 0
-p2 = 0
+p1 = p2 = 0
 
 while p1 < len1 and p2 < len2:
     if list1[p1] < list2[p2]:
@@ -15,13 +14,10 @@ while p1 < len1 and p2 < len2:
         res.append(list2[p2])
         p2 += 1
 
-while p1 < len1:
-    res.append(list1[p1])
-    p1 += 1
-
-while p2 < len2:
-    res.append(list2[p2])
-    p2 += 1
+if p1 < len1:
+    res += list1[p1:]
+else:
+    res += list2[p2:]
 
 for x in res:
     print(x, end=' ')
