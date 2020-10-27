9e81fe75666d3f8ed63d30c3fe345ad974c42ad6
CheeseB <2489ckckck@naver.com>
2020-10-27 23:11:53 +0900

    modify

--- Inflearn/hourglass.py
+++ Inflearn/hourglass.py
@@ -1,31 +1,25 @@
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
-newGrid = [[0] * n for _ in range(n)]
-for i in range(n):
-    for j in range(n):
-        newGrid[i][j] = grid[i][j]
 
 m = int(input())
 for _ in range(m):
     row, way, rotate = map(int, input().split())
     row -= 1
-    if way == 1:
-        for i in range(n):
-            col = (i + rotate) % n
-            newGrid[row][col] = grid[row][i]
+    if way == 0:
+        for _ in range(rotate):
+            # 맨앞을 빼서 맨뒤로 붙임
+            grid[row].append(grid[row].pop(0))
     else:
-        for i in range(n):
-            col = (n + i - rotate) % n
-            newGrid[row][col] = grid[row][i]
-    for j in range(n):
-        grid[row][j] = newGrid[row][j]
+        for _ in range(rotate):
+            # 맨뒤를 빼서 맨앞에 붙임
+            grid[row].insert(0, grid[row].pop())
 
 res = 0
 start = 0
 end = n
 for i in range(n):
     for j in range(start, end):
-        res += newGrid[i][j]
+        res += grid[i][j]
     if i >= n//2:
         start -= 1
         end += 1
