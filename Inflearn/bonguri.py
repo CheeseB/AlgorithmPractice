64aad877e27ed396eeb316e60cb21b127a337253
CheeseB <2489ckckck@naver.com>
2020-10-29 18:18:44 +0900

    modify

--- Inflearn/bonguri.py
+++ Inflearn/bonguri.py
@@ -1,18 +1,22 @@
 n = int(input())
-land = [[0] * (n+2) for _ in range(n+2)]
-temp = [list(map(int, input().split())) for _ in range(n)]
-for i in range(n):
-    for j in range(n):
-        land[i+1][j+1] = temp[i][j]
+land = [list(map(int, input().split())) for _ in range(n)]
+land.insert(0, [0] * n)  # 2차원 리스트 맨 첫줄에 한줄 추가
+land.append([0] * n)  # 2차원 리스트 맨 끝줄에 한줄 추가
+for line in land:
+    # 각 줄의 처음과 끝에 0 추가
+    line.insert(0, 0)
+    line.append(0)
+
+# 각 칸의 상하좌우를 비교할 수 있도록 방향 저장
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
 res = 0
 
 for i in range(1, n+1):
     for j in range(1, n+1):
-        left = land[i][j-1]
-        right = land[i][j+1]
-        up = land[i-1][j]
-        down = land[i+1][j]
-        if max(left, right, up, down) < land[i][j]:
+        # 모든 k에 대해서 조건을 만족하면 참
+        if all(land[i][j] > land[i+dx[k]][j+dy[k]] for k in range(4)):
             res += 1
 
 print(res)
