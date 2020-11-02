0f3e8597a09a6a7d91c8e9b420940c79a84ee286
CheeseB <2489ckckck@naver.com>
2020-11-02 20:27:19 +0900

    sudoku(resolved)

--- Inflearn/sudoku.py
+++ Inflearn/sudoku.py
@@ -1,16 +1,24 @@
-board = [list(map(int, input().split())) for _ in range(9)]
+def suDoKu(board):
+    for i in range(9):
+        rowList = [0] * 10
+        colList = [0] * 10
+        for j in range(9):
+            rowList[board[i][j]] = 1
+            colList[board[j][i]] = 1
+        if sum(rowList) != 9 or sum(colList) != 9:
+            return "NO"
 
-for i in range(9):
-    rowList = [0] * 10
-    colList = [0] * 10
+    for i in range(0, 9, 3):
+        for j in range(0, 9, 3):
+            groupList = [0] * 10
+            for k in range(3):
+                for s in range(3):
+                    groupList[board[i+k][j+s]] = 1
+            if sum(groupList) != 9:
+                return "NO"
 
-    for j in range(9):
-        rowList[board[i][j]] += 1
-        colList[board[j][i]] += 1
+    return "YES"
 
-    if max(rowList) > 1 or max(colList) > 1:
-        print("NO")
-        break
 
-else:
-    print("YES")
+inputBoard = [list(map(int, input().split())) for _ in range(9)]
+print(suDoKu(inputBoard))
