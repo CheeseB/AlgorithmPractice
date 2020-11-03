40a151fb8a8fe9728772a9b4a3cf93f3e4f93460
CheeseB <2489ckckck@naver.com>
2020-11-03 16:27:05 +0900

    modify

--- Inflearn/numOfPalindrome.py
+++ Inflearn/numOfPalindrome.py
@@ -3,14 +3,11 @@ res = 0
 
 for i in range(7):
     for j in range(3):
-        for k in range(2):
-            if grid[i][j+k] != grid[i][j-k+4]:
-                break
-        else:
+        # 2차원 리스트의 행 일부를 1차원 리스트로 슬라이스
+        # j부터 j+5 전까지
+        temp = grid[i][j:j+5]
+        if temp == temp[::-1]:
             res += 1
-
-for i in range(7):
-    for j in range(3):
         for k in range(2):
             if grid[j+k][i] != grid[j-k+4][i]:
                 break
