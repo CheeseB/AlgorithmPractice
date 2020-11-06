775759669dc299c1d2ba674c2a9bbbb0ddbe3116
CheeseB <2489ckckck@naver.com>
2020-11-06 15:39:00 +0900

    cuttingLan (success)

--- Inflearn/cuttingLan.py
+++ Inflearn/cuttingLan.py
@@ -1,25 +1,29 @@
+def count(length):
+    cnt = 0
+    # lanList 전역변수가 함수 선언보다 밑에 선언되어도 사용 가능함
+    for lan in lanList:
+        cnt += (lan // length)
+    return cnt
+
+
+# 여기 선언된 변수들은 전역변수로 취급되기 때문에
+# 함수의 인자로 넣어주지 않아도 사용이 가능함
 k, n = map(int, input().split())
-lanList = list()
+lanList = []
 for _ in range(k):
     lanList.append(int(input()))
+res = 1
 lt = 0
-rt = 1000
-res = 0
-while True:
-    cnt = 0
-    mid = (lt + rt) // 2
-    for lan in lanList:
-        cnt += lan // mid
-    if cnt >= n:
-        break
-    rt = mid - 1
+rt = max(lanList)
 
-while cnt >= n:
-    res = mid
-    lt = mid + 1
+while lt <= rt:
     mid = (lt + rt) // 2
-    cnt = 0
-    for lan in lanList:
-        cnt += lan // mid
+    # mid 변수는 지역변수라서
+    # 함수의 인자로 넣어줘야만 사용이 가능함
+    if count(mid) >= n:
+        res = mid
+        lt = mid + 1
+    else:
+        rt = mid - 1
 
 print(res)
