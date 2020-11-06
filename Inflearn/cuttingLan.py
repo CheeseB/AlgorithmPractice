af99232a263308368bb7c30ac0a3bded7bf811e4
CheeseB <2489ckckck@naver.com>
2020-11-06 15:09:52 +0900

    cuttingLan (fail)

--- Inflearn/cuttingLan.py
+++ Inflearn/cuttingLan.py
@@ -2,17 +2,24 @@ k, n = map(int, input().split())
 lanList = list()
 for _ in range(k):
     lanList.append(int(input()))
-length = min(lanList)
 lt = 0
-rt = length
-while lt <= rt:
+rt = 1000
+res = 0
+while True:
+    cnt = 0
+    mid = (lt + rt) // 2
+    for lan in lanList:
+        cnt += lan // mid
+    if cnt >= n:
+        break
+    rt = mid - 1
+
+while cnt >= n:
+    res = mid
+    lt = mid + 1
+    mid = (lt + rt) // 2
     cnt = 0
     for lan in lanList:
-        cnt += lan // length
-    length = (lt + rt) // 2
-    if cnt < n:
-        rt = length
-    else:
-        lt = length
+        cnt += lan // mid
 
-print(length)
+print(res)
